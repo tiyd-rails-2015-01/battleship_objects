fc92866edded305cb613f30276078bfabb48fa4a
Ashley Ellis <anellis12@gmail.com>
2015-01-14 20:12:00 -0500

    Added files for Board, Game, Player, and Ship classes as wells as a battleship_objects file

---
+++ battleship_objects.rb
@@ -0,0 +1,13 @@
+carrier = Ship.new ("carrier", 5)
+battleship = Ship.new ("battleship", 4)
+cruiser = Ship.new ("cruiser", 3)
+submarine = Ship.new ("submarine", 3)
+destroyer = Ship.new ("destroyer", 2)
+
+player1 = Player.new("player1",board1)
+player2 = Player.new("player2" board2)
+
+board1= Board.new("board1")
+board2= Board.new("board2")
+
+game= Game.new
