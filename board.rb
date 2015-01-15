0b9ba4aed8a10fe73d4c3404aff7dbb5b788acd0
Mike Marbry <mlmarbry@gmail.com>
2015-01-14 19:51:05 -0500

    Board

---
+++ board.rb
@@ -0,0 +1,21 @@
+ class Board
+   def initialize(ship, position, orientation)
+     @ship = ship
+     @position = position
+     @orientation = orientation
+   end
+
+   def ship
+     
+
+   def position
+     #farthest space occupied towards top or left
+   end
+
+   def orientation
+     #horizontal or verticle
+   end
+
+
+
+ end
